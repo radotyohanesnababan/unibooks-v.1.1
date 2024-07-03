800ecb47a195b9c3b90e87a04dd8824a78a215eb
Radot <radot2510@gmail.com>
2024-07-03 09:26:00 +0700

    register-login fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,10 +40,6 @@
       <label for="password" class="block text-gray-600">Password</label>
       <input type="password" id="password" name="password" class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-gray-500" autocomplete="off">
     </div>
-    <!-- Forgot Password Link -->
-    <div class="mb-6 text-gray-500">
-      <a href="#" class="hover:underline">Lupa Password?</a>
-    </div>
     <!-- Login Button -->
     <button type="submit" class="bg-gray-500 hover:bg-gray-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
   </form>
